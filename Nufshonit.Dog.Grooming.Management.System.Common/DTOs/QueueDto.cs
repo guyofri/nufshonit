5dd47f61f44e342fcd196f48754590a301e14ead
guyofri <32114090+guyofri@users.noreply.github.com>
2024-11-24 22:53:25 +0200

    common folder

---
+++ Nufshonit.Dog.Grooming.Management.System.Common/DTOs/QueueDto.cs
@@ -0,0 +1,10 @@
+﻿public class BarberQueueDto
+{
+    public long? Id { get; set; }
+    public string? CustomerName { get; set; }
+    public DateTime? ArrivalTime { get; set; }
+    public long? CreatedByUserId { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? FromCreatedDate { get; set; }
+    public DateTime? ToCreatedDate { get; set; }
+}
---
+++ Nufshonit.Dog.Grooming.Management.System.Common/DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+﻿public class UserDto
+{
+    public long? Id { get; set; }
+    public string? UserName { get; set; }
+    public string? PasswordHash { get; set; }
+    public string? FirstName { get; set; }
+    public string? Password { get; set; }
+    public string? Token { get; set; }
+}
